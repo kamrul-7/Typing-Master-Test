4be822939ef2f54d1baaa949ec5737e6dff0b585
kamrul-7 <kamrul7.khan@gmail.com>
2022-09-15 04:36:02 +0600

    count int number

--- script.js
+++ script.js
@@ -134,7 +134,8 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpentInt = timeSpent.toFixed(0)
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? timeSpentInt : 0} seconds`;
 }, 1000);
